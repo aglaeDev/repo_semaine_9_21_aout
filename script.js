7ba7221e0839652bc9c104c12f9fc53239f0019b
aglaeDev <a.gourdet.dev@gmail.com>
2023-08-21 21:37:21 +0200

    last commit

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() { //sert à attacher un gestionnaire d'événement au document HTML 
-  //section console
-  //selectionne le footer
-  let footer = document.querySelector('footer');
-  //cree la variable cliquenum et la met a 0
+  const footer = document.querySelector('footer');
   let cliquenum = 0;
- //ajoute a footer la fonction de cliquenum
+  const buttonnav = document.querySelector('.navbar-toggler');
+  const navbarHeader = document.getElementById('navbarHeader');
+  const cards = document.querySelectorAll('.card');
+  const navbar = document.querySelector('.navbar');
+  let isNuclearActivated = false;
+  const originalLink = document.querySelector('link[href*="bootstrap.min.css"]').cloneNode();
+  const viewButtons = document.querySelectorAll('.btn-success');
+  const moveButton = document.querySelector('.btn-secondary');
+  const cardsContainer = document.querySelectorAll('.row');
+
+
+  //ajoute a footer la fonction de cliquenum
   footer.addEventListener('click', function() {
     cliquenum++;
     console.log("click num" + cliquenum);
   });
 
-  //section collapse
-
-  // Sélection de l'élément du bouton
-  let buttonnav = document.querySelector('.navbar-toggler');
-
-  // Sélection de l'élément à modifier
-  let navbarHeader = document.getElementById('navbarHeader');
 
   // Ajout d'un gestionnaire d'événement au clic sur le bouton
   buttonnav.addEventListener('click', function() {
@@ -28,45 +29,33 @@ document.addEventListener('DOMContentLoaded', function() { //sert à attacher un
       // Sinon, la rajouter
       navbarHeader.classList.add('collapse');
     }
-  });
+  });  
 
-  //card rouge section 
 
-  //pointer la card en entier
-  let card = document.querySelector('.card');
+  Array.from(cards).forEach(function(card) {
+    // Sélection de l'élément du bouton
+    let buttonedit = card.querySelector('.btn-outline-secondary');
+    
+    // Ajout d'un gestionnaire d'événement au clic sur le bouton
+    buttonedit.addEventListener('click', function() {
+      //change la couleur du text 
+      if (card.style.color === '' || card.style.color === 'initial') {
+        card.style.color = '#df2935'; // Mettre en rouge
+      } else if (card.style.color === 'green') {
+        card.style.color = ''; // Revenir à la couleur par défaut
+      } else {
+        card.style.color = 'green'; // Mettre en vert
+      }
+    });
 
-  console.log('card')
-
-  // Sélection de l'élément du bouton
-  let buttonedit = document.querySelector('.btn-outline-secondary');
-
-  // Ajout d'un gestionnaire d'événement au clic sur le bouton
-  buttonedit.addEventListener('click', function() {
-
-    //change la couleur du text 
-    if (card.style.color === '' || card.style.color === 'initial') {
-      card.style.color = '#df2935'; // Mettre en rouge
-    } else if (card.style.color === 'green') {
-      card.style.color = ''; // Revenir à la couleur par défaut
-    } else {
-      card.style.color = 'green'; // Mettre en vert
-    }
-  });
-
-  //section nucléaire
+  })
   
-  // Sélection de la navbar en haut
-  let navbar = document.querySelector('.navbar');
-
-  // Variables pour suivre l'état
-  let isNuclearActivated = false;
-  let originalLink = document.querySelector('link[href*="bootstrap.min.css"]').cloneNode();
 
   // Ajout d'un gestionnaire d'événement au double-clic sur la navbar
   navbar.addEventListener('dblclick', function() {
     if (isNuclearActivated) {
       // Si le mode "nucléaire" est activé, rétablir l'état normal
-      document.querySelector('link[href*="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"]').remove();
+      document.querySelector("link[rel='stylesheet']");
       document.head.appendChild(originalLink);
       isNuclearActivated = false;
     } else {
@@ -76,34 +65,42 @@ document.addEventListener('DOMContentLoaded', function() { //sert à attacher un
     }
   });
 
+  function toggleCard(card) {
 
-  //section view
-  // Sélection de tous les boutons "View"
-   let viewButtons = document.querySelectorAll('.btn-success');
+    // Ajustement des styles pour réduire la taille et masquer le texte
+    card.querySelector('.card-text').classList.toggle('collapse');
 
-   // Ajout d'un gestionnaire d'événement pour le passage de la souris (mouseenter)
-   viewButtons.forEach(function(button) {
-     button.addEventListener('mouseenter', function() {
-       // Sélection de la carte parente (élément avec la classe .card) du bouton
-       let card = button.closest('.card');
- 
-       // Ajustement des styles pour réduire la taille et masquer le texte
-       card.style.transform = 'scale(0.8)';
-       card.querySelector('.card-text').style.display = 'none';
-       card.querySelector('img').style.transform = 'scale(0.2)';
-     });
- 
-     // Ajout d'un gestionnaire d'événement pour la sortie de la souris (mouseleave)
-     button.addEventListener('mouseleave', function() {
-       // Sélection de la carte parente (élément avec la classe .card) du bouton
-       let card = button.closest('.card');
- 
-       // Rétablir les styles d'origine
-       card.style.transform = '';
-       card.querySelector('.card-text').style.display = '';
-       card.querySelector('img').style.transform = '';
-     });
-   });
+    if (!card.querySelector('.card-text').classList.contains('collapse')) {
+      card.querySelector('img').style.transform = 'scale(1)';    
+    } else {
+      card.querySelector('img').style.transform = 'scale(0.2)';
+    }
+  }
+
+  // Ajout d'un gestionnaire d'événement pour le passage de la souris (mouseenter)
+  Array.from(viewButtons).forEach(function(button) {
+    // console.log("button", button);  
+
+    // Sélection de la carte parente (élément avec la classe .card) du bouton
+    const cardEl = button.closest('.card');
+
+    button.addEventListener('mouseenter', function() {
+      toggleCard(cardEl);
+    });
+  
+  });
+
+  moveButton.addEventListener('click', function() {
+    let lastCard = cardsContainer[1].lastElementChild;
+      
+    cardsContainer[1].insertBefore(lastCard, cardsContainer[1].firstElementChild);
+  });
+
+  // moveButton.addEventListener('click', function() {
+  //   let firstCard = cardsContainer[1].lastElementChild;
+      
+  //   cardsContainer[1].insertafter(firstCard, cardsContainer[1].firstElementChild);
+  // });
 });
 
 
